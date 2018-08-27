55e6ad397c607e5e664e82029ff427962edbee59
Pierre <pv@v2v.co.za>
2018-08-27 12:58:23 +0000

    update dburl

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
 
-mongoose.connect("mongodb://pierre:venter1@ds235302.mlab.com:35302/yelpcamp"); 
-//var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v10";
-//mongoose.connect(url);
+//mongoose.connect("mongodb://pierre:venter1@ds235302.mlab.com:35302/yelpcamp"); 
+var url = process.env.DATABASEURL || "mongodb://localhost:27017/yelp_camp";
+mongoose.connect(url);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
